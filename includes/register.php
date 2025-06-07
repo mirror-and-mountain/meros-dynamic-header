011601dfb5278a4e07e391fa93a0d742b3263e43
Toby Wilson <109956652+tmw-mm@users.noreply.github.com>
2025-06-07 09:46:33 +0000

    Simplify block registration

--- includes/register.php
+++ includes/register.php
@@ -1,16 +1,6 @@
 <?php
-
-function meros_dh_define_constants(): void {
-    defined('MEROS_DH_PATH') || define('MEROS_DH_PATH', dirname(__DIR__));
-}
-
 // Register the block.
-function meros_dh_register_blocks() {
-
-    $blocks_path = wp_normalize_path(MEROS_DH_PATH . '/build');
-
+add_action('init', function () {
+    $blocks_path = wp_normalize_path(dirname(__DIR__) . '/build');
     register_block_type( $blocks_path );
-}
-
-meros_dh_define_constants();
-add_action('init', 'meros_dh_register_blocks');
+});
